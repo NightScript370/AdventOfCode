b05affadc3b622e8eee573812734d4688a03336e
NightScript <18664762+NightScript370@users.noreply.github.com>
2022-12-12 16:55:36 -0500

    Day 1 - Problem 1 - 2018

--- day1-prob1.js
+++ day1-prob1.js
@@ -1,10 +1,1 @@
-const numbers = Uint16Array.from(document.body.innerText.split("\n").map(numString => parseInt(numString)))
-let increments = 0;
-console.log(numbers.length)
-for (let i = 1; i <= numbers.length; i++) {
-    if (numbers[i] > numbers[i - 1])
-        increments++;
-    console.log(i)
-}
-
-console.log(increments);
+document.body.innerText.split("\n").map(numString => parseInt(numString)).filter(Boolean).reduce((a, b) => a + b)
